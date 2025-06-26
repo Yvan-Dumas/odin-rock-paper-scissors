fdfc7f7222b3b5f8e613f24a34fcb5c2906027f0
Yvan <yvandumas05@gmail.com>
2025-06-26 16:16:08 +0200

    Add the playround function

--- javascript.js
+++ javascript.js
@@ -10,9 +10,54 @@ function getComputerChoice() {
 }
 
 function getHumanChoice() {
-    return prompt("What do you want to play?");
+  return prompt("What do you want to play?");
 }
 
-console.log(getComputerChoice());
-let choice = getHumanChoice();
-console.log(choice);
+function playRound(computerChoice, humanChoice) {
+  
+  console.log("You played " + humanChoice + " and computer played " + computerChoice + ".");
+  
+  switch (humanChoice) {
+    case "rock": 
+      if (computerChoice == "rock") {
+        console.log("tied round!");
+      } else if (computerChoice == "paper") {
+        console.log("You lose, paper beats rock!");
+        computerScore++;
+      } else {
+        console.log("You win, rock beats scissors!");
+        humanScore++;
+      }
+      break;
+
+    case "paper": 
+      if (computerChoice == "paper") {
+        console.log("tied round!");
+      } else if (computerChoice == "rock") {
+        console.log("You win, paper beats rock!");
+        humanScore++;
+      } else {
+        console.log("You lose, scissors beats paper!");
+        computerScore++;
+      }
+      break;
+
+    case "scissors": 
+      if (computerChoice == "scissors") {
+        console.log("tied round!");
+      } else if (computerChoice == "paper") {
+        console.log("You win, scissors beats paper!");
+        humanScore++;
+      } else {
+        console.log("You lose, rock beats scissors!");
+        computerScore++;
+      }
+      break;
+  }
+}
+
+let humanScore = 0;
+let computerScore = 0;
+
+const humanChoice = getHumanChoice().toLowerCase();
+const computerChoice = getComputerChoice();
